53bf8f645df1c6774e88695d4ae94071d411636e
nodwengu <thanduxolonodwengu@gmail.com>
2021-08-03 23:58:52 +0200

    Initial commit

---
+++ BootcampFunctions.Test/GreetTest.cs
@@ -0,0 +1,21 @@
+using System;
+using Xunit;
+using BootcampFunctions;
+
+namespace BootcampFunctions.Test
+{
+    public class GreetTest
+    {
+        [Fact]
+        public void ShouldBeAbleToGreetJo()
+        {            
+            Assert.Equal("Hello, Jo", Greet.Execute("Jo"));
+        }
+
+        [Fact]
+        public void ShouldBeAbleToGreetLihle()
+        {
+            Assert.Equal("Hello, Lihle", Greet.Execute("Lihle"));
+        }
+    }
+}
---
+++ BootcampFunctions/Greet.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BootcampFunctions
+{
+    public class Greet
+    {
+        public static string Execute(String name) {
+            return $"Hello, {name}";
+        }
+    }
+}
