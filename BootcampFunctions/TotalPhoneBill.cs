05c2eb7849402b43416297214d7dbc5c505d821e
nodwengu <thanduxolonodwengu@gmail.com>
2021-08-04 17:44:22 +0200

    Added more test

--- BootcampFunctions/TotalPhoneBill.cs
+++ BootcampFunctions/TotalPhoneBill.cs
@@ -4,24 +4,20 @@ namespace BootcampFunctions
 {
     public class TotalPhoneBill
     {
-         public string CalcTotalPhoneBill(string callsAndSms)
-        {
+         public string CalcTotalPhoneBill(string callsAndSms) {
             var callsAndSmsArr = callsAndSms.Split(",");
 
             double callsTotal = 0.0;
             double smsTotal = 0.0;
 
-
             foreach (var item in callsAndSmsArr) {
                if (item == "call") {
                   callsTotal += 2.75;
-               }
-               else {
+               } else {
                   smsTotal += 0.65;
                }
             }
-            // Console.WriteLine("callsTotal " + callsTotal);
-            // Console.WriteLine("smsTotal " + smsTotal);
+            
             return $"R{callsTotal + smsTotal}";
         }
     }
