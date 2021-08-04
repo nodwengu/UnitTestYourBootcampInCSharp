22eec5ebd70454a36967fcacecda4fa8001b314d
nodwengu <thanduxolonodwengu@gmail.com>
2021-08-04 23:08:28 +0200

    Added my projects to solutions

--- BootcampFunctions/TotalPhoneBill.cs
+++ BootcampFunctions/TotalPhoneBill.cs
@@ -2,23 +2,20 @@ using System;
 
 namespace BootcampFunctions
 {
-    public class TotalPhoneBill
-    {
-         public string CalcTotalPhoneBill(string callsAndSms) {
-            var callsAndSmsArr = callsAndSms.Split(",");
+   public class TotalPhoneBill {
+      public string CalcTotalPhoneBill(string callsAndSms) {
+         var callsAndSmsArr = callsAndSms.Split(",");
+         double callsTotal = 0.0;
+         double smsTotal = 0.0;
 
-            double callsTotal = 0.0;
-            double smsTotal = 0.0;
-
-            foreach (var item in callsAndSmsArr) {
-               if (item == "call") {
-                  callsTotal += 2.75;
-               } else {
-                  smsTotal += 0.65;
-               }
+         foreach (var item in callsAndSmsArr) {
+            if (item == "call") {
+               callsTotal += 2.75;
+            } else {
+               smsTotal += 0.65;
             }
-            
-            return $"R{callsTotal + smsTotal}";
-        }
-    }
+         }
+         return $"R{callsTotal + smsTotal}";
+      }
+   }
 }
